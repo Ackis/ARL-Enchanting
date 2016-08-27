9cc98b5bdb86954e2483bb3478adda596176eeb5
Ressy <resa1983@hotmail.com>
2016-08-27 10:14:28 -0400

    Updating locations

--- Quests.lua
+++ Quests.lua
@@ -34,23 +34,22 @@ function module:InitializeQuests()
 		})
 	end
 
-	AddQuest(39905,	Z.AZSUNA,			0.0,	0.0,	"Neutral") -- Ringing True  --Needs updating
-	AddQuest(39923,	Z.AZSUNA,			0.0,	0.0,	"Neutral") -- Down to the Core --Needs updating
+	AddQuest(39905,	Z.AZSUNA,			43.2,	43.6,	"Neutral")
+	AddQuest(39923,	Z.AZSUNA,			47.2,	26.4,	"Neutral")
 	AddQuest(39875, Z.DALARAN_BROKENISLES,		38.3,	41.5,	"Neutral")
-	AddQuest(39882,	Z.VALSHARAH,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(39883,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(39882,	Z.VALSHARAH,			54.4,	57.6,	"Neutral")
+	AddQuest(39883,	Z.THUNDER_TOTEM,		44.2,	45.2,	"Neutral")
 	AddQuest(39904,	Z.STORMHEIM,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(39910,	Z.AZSUNA,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(39914,	Z.AZSUNA,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(39918,	Z.AZSUNA,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(41670,	Z.VALSHARAH,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(41671,	Z.HIGHMOUNTAIN,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(41672, Z.STORMHEIM,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(41673,	Z.SURAMAR,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(41674,	Z.DALARAN_BROKENISLES,		0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(39910,	Z.AZSUNA,			46.8,	40.8,	"Neutral")
+	AddQuest(39914,	Z.AZSUNA,			46.8,	40.8,	"Neutral")
+	AddQuest(39918,	Z.AZSUNA,			46.8,	40.8,	"Neutral")
 	AddQuest(41669,	Z.AZSUNA,			0.0,	0.0,	"Neutral") --Needs updating
-	AddQuest(42971,	Z.DEEPHOLM,			46.8,	13.2,	"Neutral") --Needs updating
-	AddQuest(42973,	Z.SURAMAR,			0.0,	0.0,	"Neutral") --Needs updating
+	AddQuest(41670,	Z.VALSHARAH,			54.6,	73.2,	"Neutral")
+	AddQuest(41671,	Z.THUNDER_TOTEM,		38.4,	46.0,	"Neutral")
+	AddQuest(41672, Z.STORMHEIM,			60.2,	51.2,	"Neutral")
+	AddQuest(41673,	Z.SURAMAR,			36.8,	46.6,	"Neutral")
+	AddQuest(41674,	Z.DALARAN_BROKENISLES,		33.4,	48.0,	"Neutral")
+	AddQuest(42971,	Z.DEEPHOLM,			56.4,	12.2,	"Neutral")
 
 	self.InitializeQuests = nil
 end
