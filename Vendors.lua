aa0fe47e1d30a9e4bbda8fcca7c69e60c175fa88
Ressy <resa1983@hotmail.com>
2016-08-27 10:15:41 -0400

    Remove vendor - Vendor is a master vendor in main module

--- Vendors.lua
+++ Vendors.lua
@@ -54,7 +54,6 @@ function module:InitializeVendors()
 	AddVendor(98367,	L["Tigrid the Charmer"],		Z.STORMHEIM,			39.4,	42.5,	"Neutral")
 	AddVendor(99420,	L["Kharmeera"],				Z.AZSUNA,			47.2,	26.4,	"Neutral")
 	AddVendor(106901,	L["Sylvia Hartshorn"],			Z.VALSHARAH,			54.7,	73.2,	"Neutral")
-	AddVendor(112146,	L["First Arcanist Thalyssra"],		Z.SURAMAR,			 0.0,	 0.0,	"Neutral") -- Needs updating, including Vendor ID
 
 	self.InitializeVendors = nil
 end
